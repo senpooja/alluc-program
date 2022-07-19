55b08618513a4be37ac6151bf7481d7e69278910
senpooja <poojasen744055@gmail.com>
2022-07-19 10:58:36 +0530

    added uc-3 files

---
+++ uc-3-program/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("welcome to Employee Management app");
+int IsFullTime = 1;
+int IsPartTime = 2;
+int EmpRatePerhr = 20;
+int empHrs = 0;
+int empWage = 0;
+
+Random random = new Random();
+int EmpCheck = random.Next(0, 2);
+if (EmpCheck == IsFullTime)
+{
+    empHrs = 8;
+}
+else if (EmpCheck == IsPartTime)
+{
+    empHrs = 4;
+}
+else
+{
+    empHrs = 0;
+}
+empWage = empHrs * EmpRatePerhr;
+Console.WriteLine("Emp Wage :" + empWage);
