19eeec90e8153fa091d18969cff5ee30f9a7377f
senpooja <poojasen744055@gmail.com>
2022-07-19 11:00:40 +0530

    added uc-4 files

---
+++ uc-4-program/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("welcome to Employee Management app");
+int IsFullTime = 1;
+int IsPartTime = 2;
+int empHrs = 0;
+int EmpWage = 0;
+Random random = new Random();
+int EmpWagePerHrs = 20;
+int EmpCheck = random.Next(0, 3);
+if (EmpCheck == IsFullTime)
+    switch (EmpCheck)
+    {
+    empHrs = 8;
+}
+else if (EmpCheck == IsPartTime)
+{
+
+    empHrs = 4;
+}
+else
+{
+    empHrs = 0;
+        case 2: //IsPartTime=2//
+        empHrs = 4;
+    break;
+        case 1: //IsFullTime=1//
+        empHrs = 8;
+    break;
+    default:
+        empHrs = 0;
+    break;
+}
+EmpWage = empHrs * EmpWagePerHrs;
+Console.WriteLine("Employees wage is: " + EmpWage);
